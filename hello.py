6badf9a2af0cf6163ebae236d396d705537bca86
Kubab511 <barabasz.kuba@gmail.com>
2025-09-26 11:20:39 +0100

    Add gitignore & contact

--- hello.py
+++ hello.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return '<p>Hello, World, I am a Flask app!</p><a href="/about">About</a>'
+    return '<p>Hello, World, I am a Flask app!</p><a href="/about">About</a><p>   </p><a href="/contact">Contact</a>'
 
 @app.route("/about")
 def about():
     return '<p>This is an about page</p><a href="/">Go back</a>'
+
+@app.route("/contact")
+def contact():
+    return '<p>Contact me at: c23310371@mytudublin.ie</p><a href="/">Go back</a>'
